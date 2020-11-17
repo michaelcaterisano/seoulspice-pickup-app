a691a0e85b6ae318902d3e654d4d2ee1963c0ce8
Michael Caterisano <mcaterisano@gmail.com>
2020-11-17 11:23:39 -0500

    adding config info

---
+++ src/config/locations.js
@@ -0,0 +1,12 @@
+const locations = [
+    {
+        description: "Restaurant Name",
+        address: "123 Test St",
+        phone: "202-555-1234",
+        id: "sandbox-sq0idb-_gAzYQA9OHUgkkhh1zAJoA", // location ID from Square
+        email: "",
+        taxRate: 0.1,
+    },
+];
+
+export default locations;
