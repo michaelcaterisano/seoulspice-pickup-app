dda181edcd719b32e59a46f06f158c2811824893
Michael Caterisano <mcaterisano@gmail.com>
2020-12-14 22:15:57 -0500

    adding check for OPTIONS request in process order

--- server/process-order.php
+++ server/process-order.php
@@ -1,25 +1,28 @@
 <?php
-
-require_once('vendor/autoload.php');
-include('./_inc/_logger.php');
-require_once('models/EmailBuilder.php');
-require_once('models/OrderBuilder.php');
-require_once('models/OrderItem.php');
-
 $env = 'dev';
-
 header('Access-Control-Allow-Origin: *'); // change to "https://pickup.seoulspice.com"
 header('Access-Control-Allow-Headers: *');
 
-$response = ['success' => false, 'message' => ''];
-$formData = file_get_contents( 'php://input' );
-$data = json_decode( $formData, false); // false is "associative" array argument
+if ($_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
+  require_once('vendor/autoload.php');
+  include('./_inc/_logger.php');
+  require_once('models/EmailBuilder.php');
+  require_once('models/OrderBuilder.php');
+  require_once('models/OrderItem.php');
+  
+  $response = ['success' => false, 'message' => ''];
+  $formData = file_get_contents( 'php://input' );
+  $data = json_decode( $formData, false); // false is "associative" array argument
+  
+  include('./_inc/config.php');
+  include('./_inc/_create-order.php');
+  include('./_inc/_process-payment.php');
+  // include('./_inc/_send-email.php');
+  
+  echo json_encode( $response );
 
-include('./_inc/config.php');
-include('./_inc/_create-order.php');
-include('./_inc/_process-payment.php');
-// include('./_inc/_send-email.php');
+}
 
-echo json_encode( $response );
+
 
 
