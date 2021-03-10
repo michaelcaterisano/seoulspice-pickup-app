3c9a31e86127c5cb1fef0801dcff503248ee934f
Michael Caterisano <mcaterisano@gmail.com>
2021-03-10 18:34:06 -0500

    retrying 500 responses

--- src/config/api.service.js
+++ src/config/api.service.js
@@ -1,7 +1,17 @@
 import { ORDER_API_URL } from "./config";
 import axios from "axios";
+import axiosRetry from "axios-retry";
 
-export const orderService = axios.create({
+const orderService = axios.create({
   baseURL: ORDER_API_URL,
   timeout: 5000,
 });
+
+axiosRetry(orderService, {
+  retries: 3,
+  retryCondition: error => {
+    return error.response.status === 500;
+  },
+});
+
+export { orderService };
